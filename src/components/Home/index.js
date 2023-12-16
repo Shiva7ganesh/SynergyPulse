93c03af6150b20142bf25dbd2d2e13090c4849d8
SaiCarthikeya <saicarthik180@gmail.com>
2023-12-17 02:52:02 +0530

    home-better

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,10 @@ import { MdDashboard } from "react-icons/md";
 import { TbReportSearch } from "react-icons/tb";
 import { FaUserDoctor } from "react-icons/fa6";
 import Header from '../Header'
+import Basic from '../../res/Basic.jpg'
+import Budget from '../../res/Budget.jpg'
+import Comprehensive from '../../res/Comprehensive.jpg'
+import Premium from '../../res/Premium.jpg'
 import './index.css'
 
 const Home = () => {
@@ -30,6 +34,38 @@ const Home = () => {
                         <p className="btn-text">View our Doctors!</p>
                     </button>
                 </div>
+                <br />
+                <hr className='white' />
+                <br />
+                <div className="insurance">
+                <h1>Explore Your Health Insurance Options</h1>
+            <div className="container-insurance">
+                <div className="card cyan">
+                    <img src={Basic} alt="Insurance 1" />
+                    <h3>Plan A: Basic Coverage</h3>
+                    <p>Ideal for individuals with low healthcare needs, offering essential coverage at an affordable price.</p>
+                    <a href="#">Learn More</a>
+                </div>
+                <div className="card magenta">
+                    <img src={Comprehensive} alt="Insurance 2" />
+                    <h3>Plan B: Comprehensive Care</h3>
+                    <p>Provides broader coverage for a wider range of medical services, suitable for families or individuals with higher needs.</p>
+                    <a href="#">Get a Quote</a>
+                </div>
+                <div className="card orange">
+                    <img src={Premium} alt="Insurance 3" />
+                    <h3>Plan C: Premium Protection</h3>
+                    <p>Offers the most extensive coverage, including specialized treatments and additional benefits for optimal peace of mind.</p>
+                    <a href="#">Compare Plans</a>
+                </div>
+                <div className="card lime">
+                    <img src={Budget} alt="Insurance 4" />
+                    <h3>Plan D: Custom Choice</h3>
+                    <p>Tailor your coverage to your specific needs and budget with flexible options and add-ons.</p>
+                    <a href="#">Contact Us</a>
+                </div>
+            </div>
+            </div>
             </div>
         </>
     )
