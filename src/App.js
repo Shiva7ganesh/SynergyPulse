e705f4f49b9b7cc156d3b09dfc44a1a947954d32
SaiCarthikeya <saicarthik180@gmail.com>
2023-12-16 18:42:26 +0530

    Update lol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
 import Login from './components/Login'
 import './App.css';
 
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route Component={Login} exact path="/login"/>
+        <Route Component={Home} exact path="/" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
