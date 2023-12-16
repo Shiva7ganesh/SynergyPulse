d5a0d67ff267014f7001a38ba1adc6b7c9aa345b
SaiCarthikeya <saicarthik180@gmail.com>
2023-12-17 02:02:17 +0530

    Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,7 @@
+import { AiFillSchedule } from "react-icons/ai";
+import { MdDashboard } from "react-icons/md";
+import { TbReportSearch } from "react-icons/tb";
+import { FaUserDoctor } from "react-icons/fa6";
 import Header from '../Header'
 import './index.css'
 
@@ -7,7 +11,25 @@ const Home = () => {
         <>
             <Header />
             <div className='home-container'>
-
+                <h1>Welcome User</h1>
+                <div className='buttons-container'>
+                    <button className='big-button cyan'>
+                        <AiFillSchedule size="50px"/>
+                        <p className="btn-text">Schedule an Appointment!</p>
+                    </button>
+                    <button className='big-button magenta'>
+                        <MdDashboard size="50px"/>
+                        <p className="btn-text">View your Dashboard!</p>
+                    </button>
+                    <button className='big-button orange'>
+                        <TbReportSearch size="50px"/>
+                        <p className="btn-text">View Reports!</p>
+                    </button>
+                    <button className='big-button lime'>
+                        <FaUserDoctor size="50px"/>
+                        <p className="btn-text">View our Doctors!</p>
+                    </button>
+                </div>
             </div>
         </>
     )
